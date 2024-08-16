53f520d0e3ead9e3e4a62fd570004b220c1c1ff9
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-08-16 18:01:53 +0530

    Add: Router Configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Login } from './Authentication/Login';
+import AuthLayout from './Authentication/AuthLayout';
+import Register from './Authentication/Register';
+import ResetPassword from './Authentication/ResetPassword';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: "<h1>Welcome to StoryScribe</h1>"
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <Login />
+      },
+      {
+        path: "register",
+        element: <Register />
+      },
+      {
+        path: "reset-password",
+        element: <ResetPassword />
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
